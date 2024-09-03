8d0667b5198f6dfdff761df4a12b2c14b320212d
KalyaniK43 <127820790+KalyaniK43@users.noreply.github.com>
2024-09-03 17:06:06 +0530

    Add files via upload

---
+++ task_manager.js
@@ -0,0 +1,17 @@
+const express=require();
+const connectedDB= await() =>{
+
+  const MONGO_URL="mongodb+srv://kalyani03k:rHU80pJq7PFZu2zg@cluster0.hqfnawq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+}
+  title;{
+         title; String;
+  }
+   description ;{
+     description ; String;
+   }
+   createdAt;{
+       createdAt; String;
+   } 
+   const exports={
+      connectedDB
+   }
